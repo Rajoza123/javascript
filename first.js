bc0f5f8c6b6e37c274c0af490f4ff762b5e1bea4
Rajoza123 <ozaraj76@gmail.com>
2023-12-11 22:34:20 +0530

    javascript filr added

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("hii there");
+const product = {
+    name:"parker jotter standard Ct ball pen",
+    color:"black",
+    prince:270,
+    star:4,
+    discount:5
+
+}
+
+console.log(product.prince)
